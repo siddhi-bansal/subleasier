3bc42af04ee4bb14ab224804a625dea3a9460072
Siddhi Bansal <siddhibansal2604@gmail.com>
2024-05-08 01:20:56 -0500

    designed home page

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Subleasier'),
+      home: const MyHomePage(title: 'SUBLEASIER'),
     );
   }
 }
@@ -31,19 +31,59 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        backgroundColor: Colors.transparent,
+        title: Text(
+            widget.title,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold, // Make the title bold
+              fontSize: 42.0,
+            ),
+          ),
+          bottom: const PreferredSize(
+            preferredSize: Size.fromHeight(20.0), // Adjust the height of the subtitle area
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 0.0),
+              child: Text(
+                'making subleasing easier',
+                style: TextStyle(
+                  fontSize: 18.0, // Adjust the font size of the subtitle
+                ),
+              ),
+            ),
+          ),
       ),
-      body: Center(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: const AssetImage('images/tower.jpg'),
+            fit: BoxFit.cover,
+            // alignment: Alignment(20 / MediaQuery.of(context).size.width, 0),
+            alignment: const Alignment(0.05, 0),
+            colorFilter: ColorFilter.mode(
+              Colors.black.withOpacity(0.6), 
+              BlendMode.dstATop, // Blend mode for the color filter
+            ),
+          ),
+        ),
+      child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            const SizedBox(height: 60.0),
             ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(const Color.fromARGB(166, 191, 87, 0)),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20), // Set border radius to 0 for a rectangular button
+                  )
+                ),
+              ),
               onPressed: () {
                 Navigator.push(
                   context,
@@ -52,22 +92,38 @@ class _MyHomePageState extends State<MyHomePage> {
               },
               child: const Column(
                 children: [
+                  SizedBox(height: 30.0),
                   Text(
                     'I\'m a Sublessor',
                     textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 22.0)
+                    style: TextStyle(
+                      fontSize: 22.0,
+                      color: Colors.black
+                    )
                   ),
                   SizedBox(height: 10.0),
                   Text(
                     'Looking for a Subtenant',
                     textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 18.0),
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.black
+                    ),
                   ),
+                  SizedBox(height: 30.0),
                 ],
               ),
             ),
-            const SizedBox(height: 50.0), 
+            const SizedBox(height: 60.0), 
             ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(const Color.fromARGB(166, 191, 87, 0)),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20), // Set border radius to 0 for a rectangular button
+                  )
+                ),
+              ),
               onPressed: () {
                 Navigator.push(
                   context,
@@ -76,23 +132,32 @@ class _MyHomePageState extends State<MyHomePage> {
               },
               child: const Column(
                 children: [
+                  SizedBox(height: 30.0),
                   Text(
                     'I\'m a Sublessee',
                     textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 22.0)
+                    style: TextStyle(
+                      fontSize: 22.0,
+                      color: Colors.black
+                    )
                   ),
                   SizedBox(height: 10.0),
                   Text(
                     'Looking for an Apartment',
                     textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 18.0),
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.black
+                    ),
                   ),
+                  SizedBox(height: 30.0),
                 ],
               ),
             )
           ],
         ),
       ),
+      )
     );
   }
 }
