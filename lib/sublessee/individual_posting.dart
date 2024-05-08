e2a30a3bf1f6cb7deb66f50f64ef28cfbcfb1658
Siddhi Bansal <siddhibansal2604@gmail.com>
2024-05-08 10:46:21 -0500

    add tower background and title to all screens

---
+++ lib/sublessee/individual_posting.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class IndividualPosting extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: const Text(
+            'SUBLEASIER',
+            style: TextStyle(
+              fontWeight: FontWeight.bold, // Make the title bold
+              fontSize: 42.0,
+            ),
+          ),
+          bottom: const PreferredSize(
+            preferredSize: Size.fromHeight(20.0), // Adjust the height of the subtitle area
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 0.0),
+              child: Text(
+                'making subleasing easier',
+                style: TextStyle(
+                  fontSize: 18.0, // Adjust the font size of the subtitle
+                ),
+              ),
+            ),
+          ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: const AssetImage('images/tower.jpg'),
+            fit: BoxFit.cover,
+            // alignment: Alignment(20 / MediaQuery.of(context).size.width, 0),
+            alignment: const Alignment(0.05, 0),
+            colorFilter: ColorFilter.mode(
+              Colors.black.withOpacity(0.8), 
+              BlendMode.dstATop, // Blend mode for the color filter
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/sublessee/postings_board.dart
+++ lib/sublessee/postings_board.dart
@@ -4,13 +4,41 @@ class PostingsBoard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        title: const Text('Postings Board'),
+        backgroundColor: Colors.transparent,
+        title: const Text(
+            'SUBLEASIER',
+            style: TextStyle(
+              fontWeight: FontWeight.bold, // Make the title bold
+              fontSize: 42.0,
+            ),
+          ),
+          bottom: const PreferredSize(
+            preferredSize: Size.fromHeight(20.0), // Adjust the height of the subtitle area
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 0.0),
+              child: Text(
+                'making subleasing easier',
+                style: TextStyle(
+                  fontSize: 18.0, // Adjust the font size of the subtitle
+                ),
+              ),
+            ),
+          ),
       ),
-      body: const Center(
-        child: Text(
-          'Postings Board',
-          style: TextStyle(fontSize: 24.0),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: const AssetImage('images/tower.jpg'),
+            fit: BoxFit.cover,
+            // alignment: Alignment(20 / MediaQuery.of(context).size.width, 0),
+            alignment: const Alignment(0.05, 0),
+            colorFilter: ColorFilter.mode(
+              Colors.black.withOpacity(0.8), 
+              BlendMode.dstATop, // Blend mode for the color filter
+            ),
+          ),
         ),
       ),
     );
