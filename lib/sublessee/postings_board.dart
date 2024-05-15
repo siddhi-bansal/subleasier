604633dad61b42d675ba6ab9393f21edbf5572aa
Siddhi Bansal <siddhibansal2604@gmail.com>
2024-05-15 10:41:11 -0500

    all postings now populates with db documents

--- lib/sublessee/postings_board.dart
+++ lib/sublessee/postings_board.dart
@@ -2,50 +2,68 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import '../firestore_service.dart';
 
-class PostingsBoard extends StatelessWidget {
+List<Map<String, dynamic>> all_postings = [];
+
+class PostingsBoard extends StatefulWidget {
   @override
+  _PostingsBoardState createState() => _PostingsBoardState();
+}
+
+class _PostingsBoardState extends State<PostingsBoard> {
+  Future<void> loadPostings() async {
+    List<Map<String, dynamic>> postings = await get_all_postings_from_db();
+    setState(() {
+      all_postings = postings;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadPostings();
+  }
+
   Widget build(BuildContext context) {
-    // CALL IT HERE: get_all_postings_from_db();
     return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        title: const Text(
-          'SUBLEASIER',
-          style: TextStyle(
-            fontWeight: FontWeight.bold, // Make the title bold
-            fontSize: 42.0,
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          title: const Text(
+            'SUBLEASIER',
+            style: TextStyle(
+              fontWeight: FontWeight.bold, // Make the title bold
+              fontSize: 42.0,
+            ),
           ),
-        ),
-        bottom: const PreferredSize(
-          preferredSize:
-              Size.fromHeight(20.0), // Adjust the height of the subtitle area
-          child: Padding(
-            padding: EdgeInsets.only(bottom: 0.0),
-            child: Text(
-              'making subleasing easier',
-              style: TextStyle(
-                fontSize: 18.0, // Adjust the font size of the subtitle
+          bottom: const PreferredSize(
+            preferredSize:
+                Size.fromHeight(20.0), // Adjust the height of the subtitle area
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 0.0),
+              child: Text(
+                'making subleasing easier',
+                style: TextStyle(
+                  fontSize: 18.0, // Adjust the font size of the subtitle
+                ),
               ),
             ),
           ),
         ),
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: const AssetImage('images/tower.jpg'),
-            fit: BoxFit.cover,
-            // alignment: Alignment(20 / MediaQuery.of(context).size.width, 0),
-            alignment: const Alignment(0.05, 0),
-            colorFilter: ColorFilter.mode(
-              Colors.black.withOpacity(0.8),
-              BlendMode.dstATop, // Blend mode for the color filter
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: const AssetImage('images/tower.jpg'),
+              fit: BoxFit.cover,
+              // alignment: Alignment(20 / MediaQuery.of(context).size.width, 0),
+              alignment: const Alignment(0.05, 0),
+              colorFilter: ColorFilter.mode(
+                Colors.black.withOpacity(0.8),
+                BlendMode.dstATop, // Blend mode for the color filter
+              ),
             ),
           ),
         ),
-      ),
-    );
+      );
   }
 }
 
